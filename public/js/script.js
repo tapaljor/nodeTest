8537751a5f33f23d05799b1cdc98f35f8e477de9
Tashi Paljor <tapaljor@gmail.com>
2024-07-14 19:28:48 -0400

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,25 @@
 let searchName = document.getElementById("searchName");
+
 searchName.addEventListener('keyup', (e) => {
     e.preventDefault();
     let searchValue = searchName.value;
-    fetch(`/searchName?q=${searchValue}`) //this and following lines
-        .then( response => response.json()) //act as ajax, wthout JQuery dependency
-        .then( data => {                   //load data dynamiclaly without loading page
-            const searchResults = document.getElementById('searchResult');
+
+    fetch(`/searchName?q=${searchValue}`) // this and following lines
+        .then(response => response.json()) // act as ajax, without jQuery dependency
+        .then(data => { // load data dynamically without loading page
+            const searchResult = document.getElementById('searchResult');
             searchResult.innerHTML = '';
+
             data.forEach(item => {
-                const li = document.createElement('li');
-                const a = document.createElement('a'); //adding a a tag
+                const a = document.createElement('a'); // adding an a tag
                 a.href = `/displayOne?id=${item._id}`;
-                a.textContent = item.name + " " + item.phoneNumber;
-                li.appendChild(a);
-                searchResult.appendChild(li);
+                a.innerText = `${item.unit} ${item.name}`;
+                searchResult.appendChild(a);
             });
+            if (data.length > 0) {
+                searchResult.classList.add('visible'); // Show the search results if there are any
+            } else {
+                searchResult.classList.remove('visible'); // Hide if no results
+            }
         });
 });
